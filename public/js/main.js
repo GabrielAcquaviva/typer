9b0aadc8af4349fbd9c57045b6d755fb03b07dbb
Gabriel Acquaviva <gabrieldelara@hotmail.com>
2019-03-03 13:16:08 -0300

    Adicionando eventos

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,14 @@ var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
 
 tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function() {
+  var conteudo = campo.val();
+
+  var qtCaracteres = conteudo.length;
+  $("#contador-caracteres").text(qtCaracteres);
+
+  var qtPalavras = conteudo.split(/\S+/).length - 1;
+  $("#contator-palavras").text(qtPalavras);
+});
