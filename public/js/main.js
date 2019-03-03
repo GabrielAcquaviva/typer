48a53196afe886b50c6f58e87e7833427ae8d54f
Gabriel Acquaviva <gabrieldelara@hotmail.com>
2019-03-03 14:21:31 -0300

    GameOver

--- public/js/main.js
+++ public/js/main.js
@@ -14,3 +14,16 @@ campo.on("input", function() {
   var qtPalavras = conteudo.split(/\S+/).length - 1;
   $("#contator-palavras").text(qtPalavras);
 });
+
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function() {
+  var cronometroId = setInterval(function() {
+    tempoRestante--;
+    $("#tempo-digitacao").text(tempoRestante);
+    if (tempoRestante < 1) {
+      campo.attr("disabled", true);
+      clearInterval(cronometroId);
+    }
+  }, 1000);
+});
